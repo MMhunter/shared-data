2c44dcd449cc87483960ba9cce41da7da826043a
mmhunter <mmhuntermax@gmail.com>
2018-04-16 20:07:45 +0800

    dev

--- index.js
+++ index.js
@@ -1,19 +1,2 @@
-let handle = {
-    get: function(target, name) {
-        return name in target ?
-            target[name] :
-            37;
-    },
-    set: function (obj, prop, value) {
-        if(typeof value === 'object'){
-            obj[prop] = new Proxy(value,handle);
-        }
-        else{
-            obj[prop] = value;
-        }
-    }
-};
+const shared = require("./lib/sharedObject");
 
-let a = new Proxy({c:2},handle);
-
-console.log(a.c.e);
